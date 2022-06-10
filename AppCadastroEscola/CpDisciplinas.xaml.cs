84928d2402fe98af64be8ff29e61b4e1cc38ea08
Lucas Mota <lucasmota7@gmail.com>
2022-06-09 21:52:41 -0300

    Atualizações nos cadastros

--- AppCadastroEscola/CpDisciplinas.xaml.cs
+++ AppCadastroEscola/CpDisciplinas.xaml.cs
@@ -12,9 +12,55 @@ namespace AppCadastroEscola
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CpDisciplinas : ContentPage
     {
+
+        List<Disciplina> disciplinas = new List<Disciplina>();
+
         public CpDisciplinas()
         {
             InitializeComponent();
+
+            lstDisciplinas.ItemsSource=disciplinas;
+        }
+
+        private void sbDisciplinas_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lstDisciplinas.ItemsSource = disciplinas.Where(
+                x => x.disciplina.ToUpper().Contains(sbDisciplinas.Text.ToUpper()));
+        }
+
+        private void btnInserir_Clicked(object sender, EventArgs e)
+        {
+            Disciplina D = new Disciplina();
+            D.disciplina = txtDisciplina.Text.ToUpper();
+
+            disciplinas.Add(D);
+
+            lstDisciplinas.ItemsSource = null;
+            lstDisciplinas.ItemsSource = disciplinas;
+
+            txtDisciplina.Text = "";
+            txtDisciplina.Focus();
+        }
+
+        async void lstDisciplinas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var v = e.SelectedItem as Disciplina;
+
+            var resp = await DisplayAlert("Exclusão", "Deseja Excluir a Disciplina: " + v.disciplina + "?", "Sim", "Não");
+
+            if(resp)
+            {
+                var item = disciplinas.Find(x => x.disciplina == v.disciplina);
+                disciplinas.Remove(item);
+                lstDisciplinas.ItemsSource = null;
+                lstDisciplinas.ItemsSource= disciplinas;
+            }
+            txtDisciplina.Focus();
+        }
+
+        async void btnVoltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
         }
     }
 }
